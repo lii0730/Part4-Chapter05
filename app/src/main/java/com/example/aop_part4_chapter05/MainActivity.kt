4469351af6d6bb9f7a81daf2e480f1c3e7464c43
LeeHsss <gustjr9630@naver.com>
2021-06-07 21:44:16 +0900

    api 추가

--- app/src/main/java/com/example/aop_part4_chapter05/MainActivity.kt
+++ app/src/main/java/com/example/aop_part4_chapter05/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.example.aop_part4_chapter05
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.browser.customtabs.CustomTabsIntent
 import com.example.aop_part4_chapter05.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
 
 	private val mainBinding: ActivityMainBinding by lazy {
 		ActivityMainBinding.inflate(layoutInflater)
 	}
+
+	val job : Job = Job()
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
@@ -21,7 +28,38 @@ class MainActivity : AppCompatActivity() {
 	private fun initLoginButton() {
 		mainBinding.loginButton.setOnClickListener {
 			//TODO: github login 기능 구현
-			Toast.makeText(this, "로그인 버튼 클릭", Toast.LENGTH_SHORT).show()
+			loginGithub()
 		}
 	}
+
+	private fun loginGithub() {
+		val loginUri = Uri.Builder().scheme("https").authority("github.com")
+			.appendPath("login")
+			.appendPath("oauth")
+			.appendPath("authorize")
+			.appendQueryParameter("client_id", BuildConfig.GITHUB_CLIENT_ID)
+			.build()
+
+		CustomTabsIntent.Builder().build().also {
+			it.launchUrl(this, loginUri)
+		}
+	}
+
+	override fun onNewIntent(intent: Intent?) {
+		super.onNewIntent(intent)
+
+		intent!!.data?.getQueryParameter("code")?.let{
+			//TODO: get Access Token
+			launch(coroutineContext) {
+				getAccessToken(it)
+			}
+		}
+	}
+
+	suspend fun getAccessToken(code: String) = withContext(Dispatchers.IO) {
+		val response
+	}
+
+	override val coroutineContext: CoroutineContext
+		get() = Dispatchers.Main + job
 }
---
+++ app/src/main/java/com/example/aop_part4_chapter05/utility/RetrofitUtil.kt
@@ -0,0 +1,4 @@
+package com.example.aop_part4_chapter05.utility
+
+object RetrofitUtil {
+}
