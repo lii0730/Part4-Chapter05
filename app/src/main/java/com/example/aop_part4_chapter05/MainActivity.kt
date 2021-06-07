72921bb223a96b31b93ef3932071ec7ed1d5a17d
root <root@neodreams.co.kr>
2021-06-07 18:06:03 +0900

    초기셋팅

--- app/src/main/java/com/example/aop_part4_chapter05/MainActivity.kt
+++ app/src/main/java/com/example/aop_part4_chapter05/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.aop_part4_chapter05
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.aop_part4_chapter05.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+	private val mainBinding: ActivityMainBinding by lazy {
+		ActivityMainBinding.inflate(layoutInflater)
+	}
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
+		setContentView(mainBinding.root)
+
+		initLoginButton()
+	}
+
+	private fun initLoginButton() {
+		mainBinding.loginButton.setOnClickListener {
+			//TODO: github login 기능 구현
+			Toast.makeText(this, "로그인 버튼 클릭", Toast.LENGTH_SHORT).show()
+		}
 	}
 }
---
+++ app/src/main/java/com/example/aop_part4_chapter05/SearchReposActivity.kt
@@ -0,0 +1,11 @@
+package com.example.aop_part4_chapter05
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchReposActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_search_repos)
+	}
+}
